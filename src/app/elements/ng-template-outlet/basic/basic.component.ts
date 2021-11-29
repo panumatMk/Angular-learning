1b9bd092fcfaf71a4fd5917f601462befcce67f1
panumatMk <panumat.mk@gmail.com>
2021-11-29 10:36:45 +0700

    update

--- src/app/elements/ng-template-outlet/basic/basic.component.ts
+++ src/app/elements/ng-template-outlet/basic/basic.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
-  styleUrls: ['./basic.component.css']
+  styleUrls: ['./basic.component.less']
 })
 export class BasicComponent implements OnInit {
 
